e997f18a79f88afd4b7ca5cd4d516ab09ab76813
abou9026510 <9026510@student.zadkine.nl>
2025-06-11 11:37:14 +0200

    Add files via upload

---
+++ statistieken.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 11 jun 2025 om 10:24
+-- Serverversie: 10.4.32-MariaDB
+-- PHP-versie: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `statistieken`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `bezoekers`
+--
+
+CREATE TABLE `bezoekers` (
+  `id` int(11) NOT NULL,
+  `land` varchar(100) DEFAULT NULL,
+  `ip_adres` varchar(50) DEFAULT NULL,
+  `provider` varchar(100) DEFAULT NULL,
+  `browser` varchar(255) DEFAULT NULL,
+  `datum_tijd` datetime DEFAULT NULL,
+  `referrer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `bezoekers`
+--
+ALTER TABLE `bezoekers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `bezoekers`
+--
+ALTER TABLE `bezoekers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
